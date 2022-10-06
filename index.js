0eb621ccef9d0896448fc3568e24d3ee87d8a8e6
Emiliano Martinez <emilianomartinez1933@gmail.com>
2022-10-05 23:29:29 -0300

    intento de express

---
+++ index.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+
+class Contenedor{
+    constructor(archive){
+        this.archive= archive;
+        this.products = [];
+    }
+
+    async read(){
+        try{
+            let dB = await fs.promises.readFile(this.archive, 'utf-8')
+            return dB
+        }catch(e){
+            console.log("error")
+        }
+    }
+
+    getId(){
+        const length = this.products.length
+        if(length < 1) return 0
+        return this.products[this.products.length - 1].id
+    } 
+
+    async save(product){
+        const id = this.getId()
+        this.products.push({
+            ...product, ...{id: id + 1}
+        })
+        try{
+            await  fs.promises.writeFile(this.archive, JSON.stringify(this.products, null, 4))
+        }catch(e){
+            console.error("No se pudo guardar" + e)
+        }
+    }
+
+    getById(id){
+        let resultId = this.products.find(prod => prod.id == id)
+        return resultId
+    }
+
+    async getAll(){
+        return this.products
+    }
+
+    deleteById(id){
+        const index = this.products.findIndex(prod => prod.id == id)
+        this.products.splice(index, 1)
+    }
+
+    async deleteAll(){
+        await fs.promises.writeFile(this.archive, "[]")
+    }
+
+}
+
+const contenedor = new Contenedor("products.txt")
+
+contenedor.save({"name": "emiliano", "edad": 28 })
+contenedor.save({"name": "lucas", "edad": 29 })
+contenedor.save({"name": "peloncha", "edad": 22 })
+console.log(contenedor.getById(2))
